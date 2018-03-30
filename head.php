a076dd03da65e1edb6c7cf11c7bfb6ea223c1ad7
minhthuy30197 <minhthuy30197@gmail.com>
2018-03-30 21:41:47 +0700

    Header of website

---
+++ head.php
@@ -0,0 +1,22 @@
+<style>
+.head{
+	margin-top: 20px;
+}
+.logo {
+	float: left;
+}
+.action {
+	float: right;
+	margin-top: 100px;
+}
+</style>
+
+<div class="head">
+	<div class="logo col-md-3 col-xs-8 ">
+		<img src="Image/newlogo.png" alt="" class="img-responsive">
+	</div>
+	<div class="action">
+		<!-- Them phan xin chao nguoi dang nhap-->
+		<!-- Them chua thi la nut dang nhap va dang ki-->
+	</div>
+</div>
