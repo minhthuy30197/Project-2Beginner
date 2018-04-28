9ea2426589a0538f9ee83d423ad350d1c8400571
minhthuy30197 <minhthuy30197@gmail.com>
2018-04-28 19:46:49 +0700

    process speaking test

---
+++ inispeak.php
@@ -0,0 +1,81 @@
+<?php
+require 'db_config.php';
+if ($_POST["action"] == "getlist") {
+    $arr = [];
+    $count = 0;
+    $sql = 'select Muc, ketqua from lichsunoi,bainoi where bainoi.MaBai = lichsunoi.MaBai and  ThoiGian in (select max(ThoiGian) from lichsunoi where MaNH = 1 group by MaNH, MaBai)';
+    $rs = $mysqli->query($sql);
+    while ($row = $rs->fetch_row()) {
+        $arr[$row[0]] = $row[1];
+        $count++;
+    }
+    $highlevel = $count;
+    $sql = 'select Muc from bainoi where Muc > ' . $highlevel;
+    $rs = $mysqli->query($sql);
+    if ($rs->num_rows > 0)
+        while ($row = $rs->fetch_row()) {
+            $arr[$row[0]] = 'false';
+            $count++;
+        }
+    $sql = 'select levelspeak from nguoihoc where MaNH = 1';
+    $rs = $mysqli->query($sql);
+    $row = $rs->fetch_row();
+    $level = $row[0];
+    if (!isset($_POST["level"])) $result->type = 1;
+    else {
+        $sql = 'select MaBai from bainoi where Muc = ' . $_POST["level"];
+        $rs = $mysqli->query($sql);
+        $row = $rs->num_rows;
+        if ($row == 0) $result->type = 1;
+        else {
+            if ($level < $_POST["level"]) $result->type = 2;
+            else
+                if ($level > $_POST["level"]) $result->type = 3;
+                else $result->type = 4;
+        }
+    }
+    $result->levelspeak = $level;
+    $result->levels = $arr;
+    $result->count = $count;
+    echo json_encode($result);
+} else
+    if ($_POST["action"] == "getcontenttype2") {
+        $sql = 'select TieuDe,Transcript,NoiDung from bainoi where Muc = '.$_POST["level"];
+        $rs = $mysqli->query($sql);
+        $row = $rs->fetch_row();
+        $obj = '';
+        $obj->TieuDe = $row[0];
+        $obj->Transcript = $row[1];
+        $obj->NoiDung = $row[2];
+        echo json_encode($obj);
+    }
+    else
+        if ($_POST["action"] == "getcontenttype4") {
+            $sql = 'select TieuDe,Transcript,NoiDung,MaBai from bainoi where Muc = '.$_POST["level"];
+            $rs = $mysqli->query($sql);
+            $row = $rs->fetch_row();
+            $obj = '';
+            $obj->TieuDe = $row[0];
+            $obj->Transcript = $row[1];
+            $obj->NoiDung = $row[2];
+            $obj->MaBai = $row[3];
+            echo json_encode($obj);
+        }
+        else
+            if ($_POST["action"] == "getcontenttype3") {
+                $sql = 'select TieuDe,Transcript,NoiDung,MaBai from bainoi where Muc = '.$_POST["level"];
+                $rs = $mysqli->query($sql);
+                $row = $rs->fetch_row();
+                $obj = '';
+                $obj->TieuDe = $row[0];
+                $obj->Transcript = $row[1];
+                $obj->NoiDung = $row[2];
+                $obj->MaBai = $row[3];
+                $sql = 'select Diem,ThoiGian from lichsunoi where ThoiGian in (select max(ThoiGian) from lichsunoi where MaNH = 1 and MaBai = 4 group by MaNH, MaBai)';
+                $rs = $mysqli->query($sql);
+                $row = $rs->fetch_row();
+                $obj->Diem = $row[0];
+                $obj->Time = $row[1];
+                echo json_encode($obj);
+            }
+?>
--- speak.php
+++ speak.php
@@ -1,187 +1,139 @@
-<script>
-    (function (e, p) {
-        var m = location.href.match(/platform=(win8|win|mac|linux|cros)/);
-        e.id = (m && m[1]) ||
-            (p.indexOf('Windows NT 6.2') > -1 ? 'win8' : p.indexOf('Windows') > -1 ? 'win' : p.indexOf('Mac') > -1 ? 'mac' : p.indexOf('CrOS') > -1 ? 'cros' : 'linux');
-        e.className = e.className.replace(/\bno-js\b/, 'js');
-    })(document.documentElement, window.navigator.userAgent)
-</script>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>2Beginner</title>
     <link href="Image/hi.png" rel="icon" type="image/ico">
-    <!-- Bootstrap CSS + jQuery library -->
-    <link href="https://plus.google.com/100585555255542998765" rel="publisher">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/style1.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/jquery.js"></script>
+    <script src="js/speak.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="//www.google.com/js/gweb/analytics/autotrack.js"></script>
-    <script>
-        new gweb.analytics.AutoTrack({
-            profile: 'UA-26908291-1'
-        });
-    </script>
+    <style>
+        .list-group {
+            max-height: 300px;
+            overflow: auto;
+        }
+    </style>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 <?php include "head.php" ?>
-<div class="container-fluid">
+<div class="container-fluid main-container">
     <div class="row">
-        <div class="sidenav">
-            Danh sách bài nói
+        <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">List speaking levels</div>
+                <div class="panel-body">
+                    <div class="list-group list-group-flush" id="list-level">
+
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="content">
-            <div id="ndhoc">
-                <p>Content</p>
-            </div>
-            <p>Let practice with this exercise. Try your best.</p>
-            <div id="ktra">
-                <div id="speaker">
-                    <button id="start_listen" onclick="start_listen()"><img src="Image/audio.png"></button>
+        <div class="col-md-9">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="content">
+                        <div id="ndhoc">
+                            <p id="title">Content</p>
+                            <p id="needtolearn"></p>
+                        </div>
+                        <p>Let practice with this exercise. Try your best.</p>
+                        <div id="speaker">
+                            <button id="start_listen" onclick="start_listen()"><img src="Image/audio.png"></button>
+                        </div>
+                        <div id="ndcandoc">
+                            <p id="nd"><p>
+                        </div>
+                        <br>
+                        <div id="ktra">
+                            <div id="info">
+                                <p id="info_start">
+                                    Click on the microphone icon and begin speaking for as long as you like.
+                                </p>
+                                <p id="info_speak_now" style="display:none">
+                                    Speak now.
+                                </p>
+                                <p id="info_no_speech" style="display:none">
+                                    No speech was detected. You may need to adjust your
+                                    <a href="//support.google.com/chrome/bin/answer.py?hl=en&amp;answer=1407892">microphone
+                                        settings</a>.
+                                </p>
+                                <p id="info_no_microphone" style="display:none">
+                                    No microphone was found. Ensure that a microphone is installed and that
+                                    <a href="//support.google.com/chrome/bin/answer.py?hl=en&amp;answer=1407892">
+                                        microphone settings</a> are configured correctly.
+                                </p>
+                                <p id="info_allow" style="display:none">
+                                    Click the "Allow" button above to enable your microphone.
+                                </p>
+                                <p id="info_denied" style="display:none">
+                                    Permission to use microphone was denied.
+                                </p>
+                                <p id="info_blocked" style="display:none">
+                                    Permission to use microphone is blocked. To change, go to
+                                    chrome://settings/contentExceptions#media-stream
+                                </p>
+                                <p id="info_upgrade" style="display:none">
+                                    Web Speech API is not supported by this browser. Upgrade to <a href=
+                                                                                                   "//www.google.com/chrome">Chrome</a>
+                                    version 25 or later.
+                                </p>
+                            </div>
+                            <div id="div_start">
+                                <button id="start_button" onclick="startButton(event)"><img id="start_img"
+                                                                                            src="Image/mic.png">
+                                </button>
+                            </div>
+                            <div id="results">
+                                <span class="final" id="final_span"></span>
+                                <span class="interim" id="interim_span"></span>
+                            </div>
+                            <button name="checkspeak" class="btn btn-primary" onclick="check()">Check</button>
+                        </div>
+                        <div id="announce">
+
+                        </div>
+                    </div>
                 </div>
-                <div id="ndcandoc">
-                    <p id="nd">Hello World. My name is Panda.
-                    <p>
-                </div>
             </div>
-            <div id="info">
-                <p id="info_start">
-                    Click on the microphone icon and begin speaking for as long as you like.
-                </p>
-                <p id="info_speak_now" style="display:none">
-                    Speak now.
-                </p>
-                <p id="info_no_speech" style="display:none">
-                    No speech was detected. You may need to adjust your
-                    <a href="//support.google.com/chrome/bin/answer.py?hl=en&amp;answer=1407892">microphone
-                        settings</a>.
-                </p>
-                <p id="info_no_microphone" style="display:none">
-                    No microphone was found. Ensure that a microphone is installed and that
-                    <a href="//support.google.com/chrome/bin/answer.py?hl=en&amp;answer=1407892">
-                        microphone settings</a> are configured correctly.
-                </p>
-                <p id="info_allow" style="display:none">
-                    Click the "Allow" button above to enable your microphone.
-                </p>
-                <p id="info_denied" style="display:none">
-                    Permission to use microphone was denied.
-                </p>
-                <p id="info_blocked" style="display:none">
-                    Permission to use microphone is blocked. To change, go to
-                    chrome://settings/contentExceptions#media-stream
-                </p>
-                <p id="info_upgrade" style="display:none">
-                    Web Speech API is not supported by this browser. Upgrade to <a href=
-                                                                                   "//www.google.com/chrome">Chrome</a>
-                    version 25 or later.
-                </p>
-            </div>
-            <div id="div_start">
-                <button id="start_button" onclick="startButton(event)"><img id="start_img" src="Image/mic.png">
-                </button>
-            </div>
-            <div id="results">
-                <span class="final" id="final_span"></span>
-                <span class="interim" id="interim_span"></span>
-            </div>
-            <div class="compact marquee" id="div_language">
-                <label for="select_dialect">Choose your accent</label>
-                <select id="select_dialect">
-                </select>
-            </div>
-            <form name = "chech" action="xuli.php" method="POST">
-                <textarea hidden name="results" id="listenressult"></textarea>
-                <button name="checkspeak" class="btn btn-success" >Check</button>
-            </form>
         </div>
     </div>
     <div class="row">
-        <?php include "footer.php"; ?>
+        <?php include "footer.php" ?>
+    </div>
+</div>
+<div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-confirm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="icon-box">
+                    <i class="material-icons">&#xE876;</i>
+                </div>
+                <h4 class="modal-title">Awesome!</h4>
+            </div>
+            <div class="modal-body">
+                <p class="text-center" id="modal-mark"></p>
+                <p class="text-center" id="modal-msg">Congratulations! You have finished this level.</p>
+            </div>
+            <div class="modal-footer">
+                <a class="btn btn-success btn-block" id="modal-btn" href="">Go to next level</a>
+            </div>
+        </div>
     </div>
 </div>
 </body>
 </html>
 
 <script>
-    $('#final_span').onchange(function () {
-        $('#listenressult').val("hihi");
-    })
-    function start_listen() {
-        var msg = new SpeechSynthesisUtterance();
-        var voices = speechSynthesis.getVoices();
-        msg.voice = voices[1]; // giọng người đọc
-        msg.voiceURI = 'native';
-        msg.volume = 1; // 0 đến 1
-        msg.rate = 1; // 0.1 đến 10
-        msg.pitch = 2; // 0 đến 2
-        msg.text = document.getElementById('nd').innerHTML;
-        console.log(document.getElementById('nd').innerHTML)
-        msg.lang = select_dialect.value; // ngôn ngữ
-        speechSynthesis.speak(msg);
+    window.onload = function () {
+        getListLevels();
     }
 
-    var chrmMenuBar = new chrm.ui.MenuBar();
-    chrmMenuBar.decorate('nav');
-    var chrmLogo = new chrm.ui.Logo('logo');
-    var chrmscroll = new chrm.ui.SmoothScroll('scroll');
-    chrmscroll.init();
-
-    window.___gcfg = {lang: 'en'};
-    (function () {
-        var po = document.createElement('script');
-        po.type = 'text/javascript';
-        po.async = true;
-        po.src = 'https://apis.google.com/js/plusone.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(po, s);
-    })();
-
-    var doubleTracker = new gweb.analytics.DoubleTrack('floodlight', {
-        src: 2542116,
-        type: 'clien612',
-        cat: 'chrom0'
-    });
-    doubleTracker.track();
-
-    doubleTracker.trackClass('doubletrack', true);
-</script>
-<script>
-    var langs =
-        {
-            'en-AU': 'Australia',
-            'en-CA': 'Canada',
-            'en-IN': 'India',
-            'en-KE': 'Kenya',
-            'en-TZ': 'Tanzania',
-            'en-GH': 'Ghana',
-            'en-NZ': 'New Zealand',
-            'en-NG': 'Nigeria',
-            'en-ZA': 'South Africa',
-            'en-PH': 'Philippines',
-            'en-GB': 'United Kingdom',
-            'en-US': 'United States'
-        };
-
-    updateDialect();
-    select_dialect.selectedIndex = 11;
     showInfo('info_start');
-
-    function updateDialect() {
-        var sel = document.getElementById("select_dialect");
-        for (index in langs) {
-            var option = document.createElement("option");
-            option.value = index;
-            option.text = langs[index];
-            sel.add(option);
-        }
-    }
-
     var final_transcript = '';
     var recognizing = false;
     var ignore_onend;
@@ -193,21 +145,19 @@
         var recognition = new webkitSpeechRecognition();
         recognition.continuous = true;
         recognition.interimResults = true;
-
         recognition.onstart = function () {
             recognizing = true;
             showInfo('info_speak_now');
             start_img.src = 'Image/mic-animate.gif';
         };
-
         recognition.onerror = function (event) {
             if (event.error == 'no-speech') {
-                start_img.src = 'Image/mic.png';
+                start_img.src = 'Image/mic.gif';
                 showInfo('info_no_speech');
                 ignore_onend = true;
             }
             if (event.error == 'audio-capture') {
-                start_img.src = 'Image/mic.png';
+                start_img.src = 'Image/mic.gif';
                 showInfo('info_no_microphone');
                 ignore_onend = true;
             }
@@ -220,13 +170,12 @@
                 ignore_onend = true;
             }
         };
-
         recognition.onend = function () {
             recognizing = false;
             if (ignore_onend) {
                 return;
             }
-            start_img.src = 'Image/mic.png';
+            start_img.src = 'Image/mic.gif';
             if (!final_transcript) {
                 showInfo('info_start');
                 return;
@@ -239,15 +188,8 @@
                 window.getSelection().addRange(range);
             }
         };
-
         recognition.onresult = function (event) {
             var interim_transcript = '';
-            if (typeof(event.results) == 'undefined') {
-                recognition.onend = null;
-                recognition.stop();
-                upgrade();
-                return;
-            }
             for (var i = event.resultIndex; i < event.results.length; ++i) {
                 if (event.results[i].isFinal) {
                     final_transcript += event.results[i][0].transcript;
@@ -287,7 +229,7 @@
             return;
         }
         final_transcript = '';
-        recognition.lang = select_dialect.value;
+        recognition.lang = 'en-US';
         recognition.start();
         ignore_onend = false;
         final_span.innerHTML = '';
